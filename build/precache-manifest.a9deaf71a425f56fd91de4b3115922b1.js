c49cf1e2bcfb197394bcfa3e615d85dfebda1b7a
jellybeantatai <abhinabakar@gmail.com>
2019-02-14 15:11:12 +0530

    creating build

---
+++ build/precache-manifest.a9deaf71a425f56fd91de4b3115922b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e960d022ff8820fe10c4",
+    "url": "/contactmanager_redux/static/js/runtime~main.e960d022.js"
+  },
+  {
+    "revision": "cb0f4747a07d20028de9",
+    "url": "/contactmanager_redux/static/js/main.cb0f4747.chunk.js"
+  },
+  {
+    "revision": "8ea483589a4343df360e",
+    "url": "/contactmanager_redux/static/js/2.8ea48358.chunk.js"
+  },
+  {
+    "revision": "cb0f4747a07d20028de9",
+    "url": "/contactmanager_redux/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "8ea483589a4343df360e",
+    "url": "/contactmanager_redux/static/css/2.3fc60cb6.chunk.css"
+  },
+  {
+    "revision": "92e4b74bc0aa2490187ef3bdb1e7e0b8",
+    "url": "/contactmanager_redux/index.html"
+  }
+];
